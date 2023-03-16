2d95556d6635ce4e8da8a871d4c04e9b0ca312f3
Fatima Slimani <fatimaslimani0@icloud.com>
2023-03-16 05:16:51 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that that print the puts"
- * return: Always  0 (Success)
+ *  main -Entry point
  *
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual buzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
