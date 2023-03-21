9b81c0d6e31b26b9f72c5ddf009b08fadcbd6234
Fatima Slimani <fatimaslimani0@icloud.com>
2023-03-21 10:39:49 -0700

    format code with betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,12 +2,15 @@
 /**
 *main - prints 10 times the alphabet, in lowercase
 */
-void print_alphabet_x10(void){
+void print_alphabet_x10(void)
+{
 	int i = 0;
 	char letter;
 
-	while(i < 10){
-		for(letter = 'a'; letter <= 'z'; letter++){
+	while (i < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
 		}
 		_putchar('\n');
