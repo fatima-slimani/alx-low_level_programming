55d16009d82d6c0550ac5719da03ae5c20a52d2f
Fatima Slimani <fatimaslimani0@icloud.com>
2023-03-21 10:34:03 -0700

    fix print-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,8 +6,8 @@ void print_alphabet_x10(void){
 	int i = 0;
 	char letter;
 
-	while(i > 10){
-		for(letter = 'a'; letter >= 'z'; letter++){
+	while(i < 10){
+		for(letter = 'a'; letter <= 'z'; letter++){
 			_putchar(letter);
 		}
 		_putchar('\n');
