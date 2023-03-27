fc826c5278296b5080f999ed51218f9bc18de7ca
Fatima Slimani <fatimaslimani0@icloud.com>
2023-03-27 11:42:56 -0700

    add code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,11 @@
 #include "main.h"
 /**
-* main - Write a function that takes a pointer to an int as parameter and updates the value it points to to 98
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Bwave/Bright
+*  Return: 0 is success
 */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
