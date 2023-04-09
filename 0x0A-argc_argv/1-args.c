25c499af87b6da8da7f060c9acc6b59dc3cf1227
Fatima Slimani <fatimaslimani0@icloud.com>
2023-04-09 09:35:49 -0700

    add 0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed
+ * @argv: Array of pointers to strings representing the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int num_args = argc - 1; /* Subtracting 1 to exclude the program name */
+
+	printf("Number of arguments: %d\n", num_args);
+
+	return 0;
+}
+
