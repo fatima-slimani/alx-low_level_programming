1bb84b5e0d532d6e5ec7dc5e16f4668c719eff0a
Fatima Slimani <fatimaslimani0@icloud.com>
2023-04-13 12:06:12 -0700

    fix 0c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,17 +11,19 @@
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int i;
+	int i, size;
 
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * (max - min + 1));
+	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * (size));
 
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= max - min; i++)
+	for (i = 0; min <= max; i++)
 		ptr[i] = min++;
 
 	return (ptr);
